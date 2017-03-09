3d211fb8174d6dd7fb6f2aa0d6a7422a6ed0a46a
Leon Zhao <Leon Zhao@192.168.0.11>
2017-03-09 18:32:38 -0500

    Build the 4D Arraylist for warehouse

--- project/A3/src/phase1/Warehouse.java
+++ project/A3/src/phase1/Warehouse.java
@@ -1,6 +1,23 @@
 package phase1;
 
+import java.util.ArrayList;
+
 public class Warehouse {
-	test one
+	//this two ArrayList use to represent two Zone inside the warehouse,
+	//the order followed by aisles,racks,level and in side the level there
+	//are 30 space to put down the Fascia.
+	static ArrayList<Fascia>[][][][] ZoneA = new ArrayList [2][3][4][30];
+	static ArrayList<Fascia>[][][][] ZoneB = new ArrayList [2][3][4][30];
+	
+	private  String WarehouseName;
+	
+	/***
+	 * Constrick the warehouse
+	 */
+	public Warehouse(){};
+	
+	public Warehouse(String WarehouseName){
+		this.WarehouseName = WarehouseName;
+	};
 
 }
