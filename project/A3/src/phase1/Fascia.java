a53298512297a8b50d1c8493b1fa287393d7ddb0
Leon Zhao <Leon Zhao@192.168.0.11>
2017-03-09 18:13:58 -0500

    Add Fascia class

---
+++ project/A3/src/phase1/Fascia.java
@@ -0,0 +1,15 @@
+package phase1;
+
+public class Fascia {
+	private String Colour;
+	private String Model;
+	private String SKU;
+	
+	public Fascia(String Colour, String Model,String SKU){
+		this.Colour = Colour;
+		this.Model = Colour;
+		this.SKU = SKU;
+			
+	}
+	
+}
