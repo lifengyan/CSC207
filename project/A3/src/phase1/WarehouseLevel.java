a326da2512d6bc0f041f6219109b6539525e369e
Leon Zhao <Leon Zhao@192.168.0.11>
2017-03-09 21:41:36 -0500

    add add and remove method to WarehouseLevel.java

--- project/A3/src/phase1/WarehouseLevel.java
+++ project/A3/src/phase1/WarehouseLevel.java
@@ -8,6 +8,21 @@ import java.util.Observer;
 public class WarehouseLevel implements Observer   {
 	private ArrayList <Fascia> fasciaList = new ArrayList();
 	
+	public WarehouseLevel(){
+		
+	}
+	
+	public void add (Fascia newFascia){
+		fasciaList.add(newFascia);
+	}
+	
+	public Fascia remove(){
+		Fascia retFascia = fasciaList.get(-1);
+		fasciaList.remove(-1);
+		this.update(arg0, arg1);
+		return retFascia;
+	}
+	
 	@Override
 	public void update(Observable arg0, Object arg1) {
 		// TODO Auto-generated method stub
