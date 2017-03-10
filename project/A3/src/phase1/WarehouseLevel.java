42df68101d13a577c9ae79452cf5458703f0ff2f
Leon Zhao <Leon Zhao@192.168.0.11>
2017-03-09 21:12:52 -0500

    crate a WarehouseLevel class

---
+++ project/A3/src/phase1/WarehouseLevel.java
@@ -0,0 +1,17 @@
+package phase1;
+//this class will be store Fasscia.
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+
+public class WarehouseLevel implements Observer   {
+	private ArrayList <Fascia> fasciaList = new ArrayList();
+	
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
