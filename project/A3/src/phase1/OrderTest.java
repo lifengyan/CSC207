6c70a6f91c0d48cedbd2d753af4df68cdc262b63
lifengyan <fengyan.li@mail.utoronto.ca>
2017-03-11 16:26:39 -0500

    Update OrderTest Sequencing

---
+++ project/A3/src/phase1/OrderTest.java
@@ -0,0 +1,39 @@
+package phase1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderTest {
+
+	@Test
+	public void testOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetStatus() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetOrderCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFront() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetBack() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEqualsOrder() {
+		fail("Not yet implemented");
+	}
+
+}
