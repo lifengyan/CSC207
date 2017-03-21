6c236e77e9147c3273f140f7387a58c7e56df232
Tong Zhu <tongzhu@Tongs-MacBook-Pro.local>
2017-03-20 21:17:52 -0400

    added files to new folder

---
+++ project/src/phasetwo/Picker.java
@@ -0,0 +1,68 @@
+package phasetwo;
+
+import java.util.ArrayList;
+
+public class Picker {
+
+
+  // a array contains all the SKU that are already picked by current picker
+  private ArrayList<Integer> forkLift = new ArrayList<Integer>();
+  private ArrayList<Location> locationList = new ArrayList<Location>();
+  private Integer requestId; // set to zero when picker is not currently picking
+  private String name;
+  private Integer curr = 0;
+  private Integer currpick = 0;
+
+  // Constructor, get picker name as a string input
+  public Picker(String pickername) {
+    this.name = pickername;
+  }
+
+  // store currently pick request's 8 locations
+  public void addLocation(ArrayList<Location> locations) {
+    this.locationList = locations;
+  }
+
+  // method to add picked SKU into forkLift
+  /**
+   * Add a fascia with sku number to forklift.
+   */
+  public void addtoFolkLift(Integer sku, Warehouse currentWarehouse) {
+
+    this.forkLift.add(sku);
+    Location currLocation = locationList.get(currpick++);
+    int zone = 0;
+    if (currLocation.zone.equals("B")) {
+      zone = 1;
+    }
+    currentWarehouse.getFascia(zone, Integer.valueOf(currLocation.aisle),
+        Integer.valueOf(currLocation.rack), Integer.valueOf(currLocation.level));
+
+  }
+
+
+  // getter method to get forkLift
+  public ArrayList<Integer> getForkLift() {
+    return this.forkLift;
+  }
+
+  // getter method to get the requestID that his picker in currently working on.
+  public Integer getRequestid() {
+    return this.requestId;
+  }
+
+  // getter function to get picker's name
+  public String getName() {
+    return this.name;
+  }
+
+  // getter method to return one location at a time
+  public String getLoc() {
+    return locationList.get(curr++).toString();
+  }
+
+  public void setRequestid(int id) {
+    this.requestId = id;
+  }
+
+}
---
+++ project/src/phasetwo/PickerManager.java
@@ -0,0 +1,61 @@
+package phasetwo;
+
+import java.util.ArrayList;
+
+public class PickerManager {
+  private ArrayList<Picker> pickerList = new ArrayList<Picker>();
+  private ArrayList<Picker> freePicker = new ArrayList<Picker>();
+
+  // Method addPicker
+  public void addPicker(Picker pickerName) {
+    this.pickerList.add(pickerName);
+
+  }
+
+  // Method to return a list of pickers that are ready, the first one in the list should be the
+  // first one called next time.
+  public ArrayList<Picker> getFreePicker() {
+    return this.freePicker;
+  }
+
+  //
+  public void addFreePicker(Picker name) {
+    this.freePicker.add(name);
+  }
+
+  // After a picker finished picking for the current request, remove this picker from the
+  public void deletFreePicker(Picker picker) {
+    this.freePicker.remove(picker);
+  }
+
+
+
+  // according to the input string to return this picker
+  /**
+   * Get picker.
+   * @param pickerName the name of the picker
+   * @return a picker
+   */
+  public Picker getPicker(String pickerName) {
+    for (int i = 0; i < pickerList.size(); i++) {
+      if (pickerList.get(i).getName().equals(pickerName)) {
+        return pickerList.get(i);
+      }
+
+    }
+    System.out.println("Given picker does not exist");
+    return null;
+  }
+
+
+  // store new picker in pickerList
+  public void storePicker(String pickerName) {
+    Picker newPicker = new Picker(pickerName);
+    pickerList.add(newPicker);
+  }
+
+  // After a picker finished picking for the current request, remove this picker from the
+  public void deletPicker(Picker picker) {
+    this.pickerList.remove(picker);
+  }
+}
