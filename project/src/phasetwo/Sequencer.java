3ca0fc78f338e6b41fb7f26c93e5cd30d29c7f68
lifengyan <fengyan.li@mail.utoronto.ca>
2017-03-21 21:47:31 -0400

    Add method finish()

--- project/src/phasetwo/Sequencer.java
+++ project/src/phasetwo/Sequencer.java
@@ -32,17 +32,18 @@ public class Sequencer extends Worker {
     int in = 3;
     while (in > -1) {
       if (om.getOrders().get(id - in).getFront() == cursku) {
-        frontpallet.add(3-in, cursku);
+        frontpallet.add(cursku);
         this.count++;
         return true;
       } else if (om.getOrders().get(id - in).getBack() == cursku){
-        backpallet.add(3-in, cursku);
+        backpallet.add(cursku);
         this.count++;
         return true;
       } else {
         in--;
       }
     }
+    //sequencer sendback id; ordermanager repick;
     return false;  
   }
   
@@ -53,6 +54,24 @@ public class Sequencer extends Worker {
     backpallet = new ArrayList<Integer>();
     return this.count;
   }
+  
+  public ArrayList<ArrayList<Integer>> sequencing(OrderManager om) {
+    frontpallet = new ArrayList<Integer>();
+    backpallet = new ArrayList<Integer>();
+    ArrayList<ArrayList<Integer>> whole = new ArrayList<ArrayList<Integer>>();
+    int in = 3;
+    while (in > -1) {
+      frontpallet.add(om.getOrders().get(id - in).getFront());
+      backpallet.add(om.getOrders().get(id - in).getBack());
+        in--;
+    }
+    whole.add(frontpallet);
+    whole.add(backpallet);
+    return whole;
+    
+    }
+    
+  
   
   
 }
