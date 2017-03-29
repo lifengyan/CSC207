9f1b4b9c86a31f20cc0abf89b413014c3e19ec53
lifengyan <fengyan.li@mail.utoronto.ca>
2017-03-29 19:01:54 -0400

    Generate new test case

---
+++ project/src/phasetwo/PickerManagerTest.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package phasetwo;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author lifengyan
+ *
+ */
+public class PickerManagerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link phasetwo.PickerManager#getORaddPicker(java.lang.String)}.
+	 */
+	@Test
+	public void testGetORaddPicker() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link phasetwo.PickerManager#getFreePicker()}.
+	 */
+	@Test
+	public void testGetFreePicker() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link phasetwo.PickerManager#addFreePicker(phasetwo.Picker)}.
+	 */
+	@Test
+	public void testAddFreePicker() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link phasetwo.PickerManager#deletFreePicker(phasetwo.Picker)}.
+	 */
+	@Test
+	public void testDeletFreePicker() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link phasetwo.PickerManager#deletPicker(phasetwo.Picker)}.
+	 */
+	@Test
+	public void testDeletPicker() {
+		fail("Not yet implemented");
+	}
+
+}
