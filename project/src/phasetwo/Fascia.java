fc2806448e4a251148db35a88c75269b15b6d764
Leon ' pc <Leon ' pc@100.64.239.20>
2017-03-30 17:15:34 -0400

    fix git bug

--- project/src/phasetwo/Fascia.java
+++ project/src/phasetwo/Fascia.java
@@ -1,24 +1,13 @@
 package phasetwo;
 
 public class Fascia {
-<<<<<<< HEAD
+
   private String sku;
-
-  public Fascia(String sku) {
-    this.sku = sku;
-  }
-
-  public Fascia() {
-
-  }
-
-  public String getsku() {
-=======
-  private int sku=1;
+  
 /***
  * constuctor if has a sku
  */
-  public Fascia(int sku) {
+  public Fascia(String sku) {
     this.sku = sku;
   }
 /***
@@ -28,8 +17,8 @@ public class Fascia {
   /***
    * return the sku
    */
-  public int getsku() {
->>>>>>> 985472fc3aefb540841089272e6c3304d462d077
+  public String getsku() {
+
     return this.sku;
   }
 }
