f5b0c3ac71edf2b01df8d464b63877d47150c17f
lifengyan <fengyan.li@mail.utoronto.ca>
2017-03-30 22:43:30 -0400

    Add replenisher unit test

--- project/src/phasetwo/Replenisher.java
+++ project/src/phasetwo/Replenisher.java
@@ -2,35 +2,37 @@ package phasetwo;
 
 public class Replenisher implements Worker {
 
-  private  String name;
+  private String name;
 
   public Replenisher(String name) {
-    this.name = name;    // TODO Auto-generated constructor stub
+    this.name = name; 
   }
 
   public String replenish(Warehouse wh, String[] userinput) {
-    Integer zone =0;
-    if (userinput[3].equals("B")){
-      zone =1;
+    Integer zone = 0;
+    if (userinput[3].equals("B")) {
+      zone = 1;
     }
     Integer aile = Integer.valueOf(userinput[4]);
     Integer rack = Integer.valueOf(userinput[5]);
     Integer level = Integer.valueOf(userinput[6]);
-    wh.getLevel(zone,aile,rack,level).set(30);
-    return userinput[3]+userinput[4]+userinput[5]+userinput[6]+ 
-        " has replenised, it has 30 Fasica now";
+    wh.getLevel(zone, aile, rack, level).set(30);
+    return userinput[3] + userinput[4] + userinput[5] + userinput[6]
+        + " has replenised, it has 30 Fasica now";
   }
-/**
- * return Replenisher`s name
- */
+
+  /**
+   * return Replenisher`s name
+   */
   @Override
   public String getName() {
     // TODO Auto-generated method stub
     return name;
   }
-/***
- *reutrn worker`s id 
- */
+
+  /***
+   * reutrn worker`s id
+   */
   @Override
   public int getid() {
     // TODO Auto-generated method stub
