6257eb8d4552f4a4ac1be53141fe24b9c2a5db32
lifengyan <fengyan.li@mail.utoronto.ca>
2017-03-30 22:04:56 -0400

    add duties

--- project/src/phasetwo/OrderCommand.java
+++ project/src/phasetwo/OrderCommand.java
@@ -30,10 +30,10 @@ public class OrderCommand implements Cammand {
    * picker
    */
   private static void createorder(OrderManager orderManager, PickerManager pickerManager,
-      WarehousePicking warehousePicking, Translate translateA, Logger LOGGER, String[] userInput)
+      WarehousePicking warehousePicking, Translate translateA, Logger logger, String[] userInput)
       throws IOException {
     orderManager.addOrder(userInput[1], userInput[2], translateA);
-    LOGGER.log(Level.FINE, "New order " + userInput[1] + " " + userInput[2] + " has been created.");
+    logger.log(Level.FINE, "New order " + userInput[1] + " " + userInput[2] + " has been created.");
 
     // check if there is free picker && there are 4 orders to generate a request
     if (pickerManager.getFreePicker().size() != 0 && orderManager.freeOrderNumber() != 0) {
@@ -41,7 +41,7 @@ public class OrderCommand implements Cammand {
       Picker curFreePicker = pickerManager.getFreePicker().get(0);
       pickerManager.deletFreePicker(curFreePicker);
       String pickerName = curFreePicker.getName();
-      LOGGER.log(Level.FINE,
+      logger.log(Level.FINE,
           "currently free picker " + pickerName + " recieve the picking request");
       // assign the free picker with the order
       HashMap<Integer, Order> newOrderMap = orderManager.generatePick();
@@ -49,7 +49,7 @@ public class OrderCommand implements Cammand {
           .addLocation(warehousePicking.optimize(warehousePicking.pickRequest(newOrderMap)));
       curFreePicker.setRequestid(orderManager.hasNext());
     } else if (pickerManager.getFreePicker().size() == 0) {
-      LOGGER.log(Level.FINE, "Currently not enough free picker or order for picking");
+      logger.log(Level.FINE, "Currently not enough free picker or order for picking");
     }
   }
 }
